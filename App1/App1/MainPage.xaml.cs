672e5ab6103b6c0f705054e0e01493c688e95b67
DanilDerypaska <82759878+DanilDerypaska@users.noreply.github.com>
2021-06-03 12:05:02 +0300

    first commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_clicked(object sender, EventArgs e) 
+        {
+            Label1.Text = Entry1.Text;
+
+        }
+        private async void Button2_clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Level1Page());
+        }
+        private void Button1_clicked(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
